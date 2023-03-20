6b374d3d02952d89904f011a2855d46601cf4f5e
Erick Mora <21460484@colima.tecnm.mx>
2023-03-20 02:10:14 -0600

    10-EX

---
+++ Ejercicio10.py
@@ -0,0 +1,32 @@
+def main():
+  print("Ejercicio 10")
+  fname = input('Enter File: ')
+  if len(fname) < 1: fname = 'clown.txt'  
+  hand = open(fname)
+
+  di = dict()
+  for lin in hand: 
+    lin = lin.rstrip()
+    wds = lin.split()
+    for w in wds:
+      #idiom: retieve/create/update counter
+      di[w] = di.get(w,0) + 1
+      
+#print(di)      
+
+#now we want to find the most common word  
+  print(di)
+  tmp = list()
+  for k,v in di.items(): 
+    #print(k,v)
+    newt = (v,k)
+    tmp.append(newt)
+
+  #print("Flipped",tmp)  
+  tmp = sorted(tmp,reverse=True)
+  #print("Sorted",tmp[:5])
+
+  for v,k in tmp[:5]:
+    print(k,v)
+if __name__ == "__main__":
+  main()
